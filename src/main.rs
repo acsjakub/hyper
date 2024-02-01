cb744452bedcd4f1029d50f834ff932bf357472d
Jakub Acs <acsjakub@gmail.com>
2024-02-01 22:40:40 +0100

    Move Segment to separate module (file)
    .. to improve code organization. Other structures will follow.
    
    Signed-off-by: Jakub Acs <acsjakub@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,9 @@
 use std::fmt;
 use std::fs::{read_to_string, File};
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Write};
+use crate::segment::Segment;
 
-struct Segment {
-    id: u64,
-    name: String,
-    address: u64,
-    len: usize,
-    flags: String,
-}
-
-impl Segment {
-    fn from_line(line: String, id: u64) -> Self {
-        let mut it = line.split(' ');
-        Self {
-            id: id,
-            name: it.next().unwrap().into(),
-            address: u64::from_str_radix(it.next().unwrap(), 16).unwrap(),
-            len: it.next().unwrap().parse::<u64>().unwrap() as usize,
-            flags: it.next().unwrap().into(),
-        }
-    }
-}
-
-impl fmt::Display for Segment {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{} {:x} {} {}",
-            self.name, self.address, self.len, self.flags
-        )
-    }
-}
+mod segment;
 
 struct Symbol {
     id: u64,
@@ -130,24 +102,6 @@ mod tests {
         let symbol_line = String::from("name deadbeef 2 RD");
         let symbol = Symbol::from_line(symbol_line.clone(), 1);
         assert_eq!(format!("{}", symbol), symbol_line);
-    }
-
-    #[test]
-    fn test_segment_from_line() {
-        let segment_line = String::from(".seg 1337dead 4000 RW");
-        let segment = Segment::from_line(segment_line, 3);
-        assert_eq!(segment.id, 3);
-        assert_eq!(segment.name, ".seg");
-        assert_eq!(segment.address, 0x1337dead);
-        assert_eq!(segment.len, 4000);
-        assert_eq!(segment.flags, "RW");
-    }
-
-    #[test]
-    fn test_segment_display() {
-        let segment_line = String::from(".seg 1337dead 4000 RW");
-        let segment = Segment::from_line(segment_line.clone(), 3);
-        assert_eq!(format!("{}", segment), segment_line);
     }
 
     #[test]
---
+++ src/segment.rs
@@ -0,0 +1,56 @@
+use std::fmt;
+
+pub struct Segment {
+    id: u64,
+    name: String,
+    address: u64,
+    len: usize,
+    flags: String,
+}
+
+impl Segment {
+    pub fn from_line(line: String, id: u64) -> Self {
+        let mut it = line.split(' ');
+        Self {
+            id: id,
+            name: it.next().unwrap().into(),
+            address: u64::from_str_radix(it.next().unwrap(), 16).unwrap(),
+            len: it.next().unwrap().parse::<u64>().unwrap() as usize,
+            flags: it.next().unwrap().into(),
+        }
+    }
+}
+
+impl fmt::Display for Segment {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} {:x} {} {}",
+            self.name, self.address, self.len, self.flags
+        )
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_segment_from_line() {
+        let segment_line = String::from(".seg 1337dead 4000 RW");
+        let segment = Segment::from_line(segment_line, 3);
+        assert_eq!(segment.id, 3);
+        assert_eq!(segment.name, ".seg");
+        assert_eq!(segment.address, 0x1337dead);
+        assert_eq!(segment.len, 4000);
+        assert_eq!(segment.flags, "RW");
+    }
+
+    #[test]
+    fn test_segment_display() {
+        let segment_line = String::from(".seg 1337dead 4000 RW");
+        let segment = Segment::from_line(segment_line.clone(), 3);
+        assert_eq!(format!("{}", segment), segment_line);
+    }
+
+}
