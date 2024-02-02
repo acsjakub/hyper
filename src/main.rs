f2abf7fa1e8bdf7559188b9df9328fd2fb0632f5
Jakub Acs <acsjakub@gmail.com>
2024-02-02 21:17:35 +0100

    Move Relocation to separate file
    Signed-off-by: Jakub Acs <acsjakub@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -3,56 +3,11 @@ use std::fs::{read_to_string, File};
 use std::io::{BufRead, BufReader, Write};
 use crate::segment::Segment;
 
+mod relocation;
 mod segment;
-struct Relocation {
-    loc: u64,
-    seg_id: u64,
-    reference: u64,
-    typ: String,
-}
-
-impl Relocation {
-    fn from_line(line: String) -> Self {
-        let mut it = line.split(' ');
-        Self {
-            loc: it.next().unwrap().parse::<u64>().unwrap(),
-            seg_id: it.next().unwrap().parse::<u64>().unwrap(),
-            reference: it.next().unwrap().parse::<u64>().unwrap(),
-            typ: it.next().unwrap().into(),
-        }
-    }
-}
-
-impl fmt::Display for Relocation {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{} {} {} {}",
-            self.loc, self.seg_id, self.reference, self.typ
-        )
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_relocation_from_line() {
-        let rel_line = String::from("1 2 3 RD");
-        let rel = Relocation::from_line(rel_line);
-        assert_eq!(rel.loc, 1);
-        assert_eq!(rel.seg_id, 2);
-        assert_eq!(rel.reference, 3);
-        assert_eq!(rel.typ, "RD");
-    }
-
-    #[test]
-    fn test_relocation_display() {
-        let rel_line = String::from("1 2 3 RD");
-        let rel = Relocation::from_line(rel_line.clone());
-        assert_eq!(format!("{}", rel), rel_line);
-    }
 
     #[test]
     fn test_object_file_load() {
